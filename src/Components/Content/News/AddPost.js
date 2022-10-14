8ede045671766777ced5e788cdea8ccff90f8e96
Gor <gmejlumyan93@gmail.com>
2022-10-14 07:44:03 -0400

    fix

--- src/Components/Content/News/AddPost.js
+++ src/Components/Content/News/AddPost.js
@@ -1,9 +1,6 @@
 import { useState } from "react";
-import { Link, Redirect } from "react-router-dom";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
-
-
 
 
 const AddPost = () => {
@@ -30,16 +27,16 @@ const AddPost = () => {
 
     return(
         <div>
-      <form>
-        <label> Title</label>
-        <input type = "text"  value = {title} placeholder='title' onChange={(e) => setTitle(e.target.value)}/>
+          <form>
+            <label> Title</label>
+            <input type = "text"  value = {title} placeholder='title' onChange={(e) => setTitle(e.target.value)}/>
         <hr />
-        <label>Body</label>
-        <input type = "text"  value = {body} placeholder='body'onChange={(e) => setBody(e.target.value)}/>
+            <label>Body</label>
+            <input type = "text"  value = {body} placeholder='body'onChange={(e) => setBody(e.target.value)}/>
         <hr />
-        <button onClick={postData}> Add Post </button>
-      </form>
-      <hr></hr>
+            <button onClick={postData}> Add Post </button>
+          </form>
+        <hr></hr>
       </div>
     )
 }
