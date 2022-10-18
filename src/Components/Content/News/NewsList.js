b73e2d85f460196be34752035daa34fb7d12ac29
Gor <gmejlumyan93@gmail.com>
2022-10-18 07:16:32 -0400

    fix

--- src/Components/Content/News/NewsList.js
+++ src/Components/Content/News/NewsList.js
@@ -1,21 +1,35 @@
+import './news.css';
 const NewsList = ({posts, title, handleDelete}) => {
 
     return (
-        <div className="news-list">
-            <h2>{title}</h2>
-            {posts.map((post) =>(
-                <div key = {post.id}> 
-                <h2 className="post-title">{post.title}</h2>
-                <p className=" post-body"> {post.body}</p>
-                
-                <button onClick={() => handleDelete(post.id)}> Delete News</button> 
-                
-                
-                </div>
-            ))}
-            
-        </div>
-
+        <>
+            <h2 className="title-name">{title}</h2>
+            <table>
+                <tbody>
+                    <tr>
+                        <th className='news-anun'>Title</th>
+                        <th className='news-title'>Body</th>
+                        <th className='news-title'></th>
+                        <th className='news-title'>Delete</th>
+                    </tr>
+                    {posts.map((post) =>( 
+                        <tr className='list-id' key={post.id}>
+                            <td>{post.title}</td>
+                            <td>{post.body} </td>
+                            <td></td>
+                            <td>
+                                <button 
+                                    onClick={() => handleDelete(post.id)} 
+                                    className='trash-box'
+                                >
+                                    Delete News
+                                </button>
+                            </td>
+                        </tr>
+                    ))} 
+                </tbody>
+            </table>
+        </>
     )
 }
 
