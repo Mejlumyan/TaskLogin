084c1fbe27f21342307bdf9332ddb5eb09423b2e
Gor <gmejlumyan93@gmail.com>
2022-10-24 07:00:47 -0400

    fix

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,31 +1,31 @@
 import { Link } from "react-router-dom";
 
-function Header(){
+function Header() {
 
   const login = localStorage.getItem('login');
 
-  return(
+  return (
     <header className='link-header'>
-    { 
-    <div>
-        <div><Link to="/home">Home</Link></div>
-        <div><Link to="/news">News</Link></div>
-        <div><Link to="/profile">Profile</Link></div>
+      {
         <div>
-        {(!login) ?  (                                
-                <Link to="/login"> Login</Link>
-            ): (
-                <Link to="/logout">Logout</Link>
+          <div><Link to="/home">Home</Link></div>
+          <div><Link to="/news">News</Link></div>
+          <div><Link to="/profile">Profile</Link></div>
+          <div>
+            {(!login) ? (
+              <Link to="/login"> Login</Link>
+            ) : (
+              <Link to="/logout">Logout</Link>
             )}
-          
+
           </div>
-       
-        <hr></hr>
-    </div>
-    }
-    
-    <hr></hr>
-   
+
+          <hr></hr>
+        </div>
+      }
+
+      <hr></hr>
+
     </header>
   );
 };
